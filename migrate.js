bd078e863ec925af2317441a579201153ae00702
Dasha <daria.tolokontceva@gmail.com>
2019-03-25 16:40:10 +0200

    migrate help

--- migrate.js
+++ migrate.js
@@ -1,3 +1,9 @@
+console.log(
+    "Commands:\n",
+    "create new         migrate create [name]\n",
+    "+all migrations    migrate migrate\n",
+    "-1 migration       migrate rollback\n")
+
 const root = require('app-root-path');
 require('sql-migrations').run({
     migrationsDir: './migrations',
