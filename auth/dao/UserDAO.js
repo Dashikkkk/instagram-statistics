985e9a0c1aea22f0461c4d719d4b1d37dbcd89f7
Dasha <daria.tolokontceva@gmail.com>
2019-03-29 16:53:59 +0200

    dont need serialize, i can control the flow byself

--- auth/dao/UserDAO.js
+++ auth/dao/UserDAO.js
@@ -13,30 +13,28 @@ class UserDAO {
      * @returns {Promise<void>}
      */
     async saveUserInfo(instagramId, values) {
-        this._db.getHandler().serialize(async () => {
-            const currentTime = moment().unix();
-            const result = await this._db.single(
-                'select id from user where instagram_id = ?',
-                [instagramId]
-            );
+        const currentTime = moment().unix();
+        const result = await this._db.single(
+            'select id from user where instagram_id = ?',
+            [instagramId]
+        );
 
-            if (result) {
-                return await this._db.execute(
-                    'update user set name = ?, full_name = ?, ip = ?, access_token = ?, \
-                        last_login = ?, updated_at = ? where id = ?',
-                    [values.userName, values.fullName, values.accessToken,
-                        values.ip, currentTime, currentTime, result.id]
-                )
-            } else {
-                return await this._db.execute(
-                    'insert into user(instagram_id, name, full_name, ip, access_token, \
-                        last_login, created_at, updated_at) values \
-                        (?, ?, ?, ?, ?, ?, ?, ?)',
-                    [instagramId, values.userName, values.fullName, values.ip,
-                        values.accessToken, currentTime, currentTime, currentTime]
-                );
-            }
-        });
+        if (result) {
+            return await this._db.execute(
+                'update user set name = ?, full_name = ?, ip = ?, access_token = ?, \
+                    last_login = ?, updated_at = ? where id = ?',
+                [values.userName, values.fullName, values.accessToken,
+                    values.ip, currentTime, currentTime, result.id]
+            )
+        } else {
+            return await this._db.execute(
+                'insert into user(instagram_id, name, full_name, ip, access_token, \
+                    last_login, created_at, updated_at) values \
+                    (?, ?, ?, ?, ?, ?, ?, ?)',
+                [instagramId, values.userName, values.fullName, values.ip,
+                    values.accessToken, currentTime, currentTime, currentTime]
+            );
+        }
     }
 }
 
